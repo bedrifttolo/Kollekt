36b85ce2b7a797627d437381e9f7ce1a65092077
Thomas Tolo <thomastj278@gmail.com>
2026-07-07 22:49:16 +0200

    Small fix to bugs in mobile app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './i18n';
 import './styles/globals.css';
 import { initNativeShell } from './lib/nativeBootstrap';
 import { ThemeProvider } from './context/ThemeContext';
+import { queryClient } from './lib/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
